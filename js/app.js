20e1ed08322660e0eb8ce33ac5a32dcffb1636cc
MarushyTha <94020902+marcythany@users.noreply.github.com>
2024-04-04 01:58:18 -0300

    style: Update CSS for improved layout and styling
    Detailed overview of changes and additions made to the CSS code:
    
    - Added Google Fonts Import for "Inconsolata" and "Karla".
    - Defined Root Variables for color schemes, font sizes, etc.
    - Applied a General Reset to remove default margins, paddings, etc.
    - Set up Global Typography for the entire document.
    - Defined Selection Color for selected text.
    - Styled Page Layout with flex container and specific properties.
    - Styled Header with specific color, font size, and transition effect.
    - Styled Footer as a flex container with alignment and padding.
    - Styled Form Container as a flex container with gap and alignment.
    - Styled Input Elements with specific colors, font size, padding, etc.
    - Styled Buttons, Select Elements, and Input Containers.
    - Defined Hover and Focus States for input elements.
    - Styled Autofilled Input Elements with background and text color.
    - Styled Focused Input Elements with background and text color.
    - Added Specific Styles for #link-input.
    - Ensured text color is applied to autofilled text.
    - Styled Links with specific colors and transition effects.
    - Defined Error Styles for input elements.
    - Applied Additional Styles for paragraphs and list items.
    - Implemented Responsive Styles for screens up to 800 pixels.
    
    These changes enhance responsiveness, layout, and styling across various screen sizes.

--- js/app.js
+++ js/app.js
@@ -11,12 +11,56 @@ async function getLinkInfo(shortenedLink) {
   try {
     const response = await fetch(apiUrl);
     if (response.ok) {
-      return await response.json(); // Simplified return statement
+      return await response.json();
     } else {
       throw new Error("Failed to fetch link info");
     }
   } catch (error) {
     console.error("Error getting link information:", error.message);
+    return null;
+  }
+}
+
+// Function to generate a shortened link
+async function generateShortenedLink(originalLink, generator, metadata) {
+  const apiUrl = "https://owo.vc/api/v2/link";
+
+  const requestBody = JSON.stringify({
+    link: originalLink,
+    generator: generator,
+    metadata: metadata,
+  });
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: requestBody,
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      if (data && data.id) {
+        let shortenedLink = data.id;
+        // Prepend protocol if missing
+        if (
+          !shortenedLink.startsWith("http://") &&
+          !shortenedLink.startsWith("https://")
+        ) {
+          shortenedLink = "https://" + shortenedLink;
+        }
+        return shortenedLink; // Return the shortened link
+      } else {
+        console.error("Shortened link not found in response data");
+        return null;
+      }
+    } else {
+      throw new Error("Failed to generate shortened link");
+    }
+  } catch (error) {
+    console.error("Error generating shortened link:", error.message);
     return null;
   }
 }
@@ -27,54 +71,229 @@ async function handleSubmit(event) {
 
   const form = event.target; // Get the form element
   const input = form.querySelector(".link-input");
+  const linkInput = form.querySelector(".link-input");
   const link = input.value.trim(); // Get the input value and trim whitespace
 
   if (!link) return; // If the input is empty, do nothing
 
+  const generatorSelect = form.querySelector("#generator");
+  const metadataSelect = form.querySelector("#metadata");
+
+  const generator = generatorSelect.value;
+  const metadata = metadataSelect.value;
+
   try {
-    const linkInfo = await getLinkInfo(link); // Fetch link information from the API
-    if (linkInfo) {
-      // If link information is successfully fetched, display it
-      console.log("Link Information:", linkInfo);
+    const shortenedLink = await generateShortenedLink(
+      link,
+      generator,
+      metadata
+    );
+
+    if (shortenedLink) {
+      linkInput.value = shortenedLink; // Update the input value with the shortened link
+      // Enable the copy button
+      const copyButton = form.querySelector("#input-copy-btn");
+      copyButton.disabled = false;
+
+      // Add event listener to copy button
+      copyButton.addEventListener("click", () => {
+        // Copy the shortened link to the clipboard
+        navigator.clipboard
+          .writeText(shortenedLink)
+          .then(() => {
+            console.log("Shortened link copied to clipboard:", shortenedLink);
+            // Optionally, you can provide user feedback here
+          })
+          .catch((error) => {
+            console.error("Error copying shortened link to clipboard:", error);
+          });
+      });
     } else {
-      // If there's an error fetching link information, display an error message
-      console.error("Failed to fetch link information.");
+      console.error("Failed to generate shortened link.");
     }
   } catch (error) {
-    // If there's an error, log it
     console.error("Error:", error.message);
   }
 }
 
-// Wait for the DOM content to be fully loaded before executing the script
 document.addEventListener("DOMContentLoaded", function () {
-  // Select the form and submit button
-  const form = document.querySelector(".link-form");
-  const submitBtn = document.getElementById("submit-btn");
+  // Create header element
+  const header = document.createElement("header");
+  header.classList.add("title");
 
-  // Add event listener to form submission
-  form.addEventListener("submit", handleSubmit);
+  // Create h1 element
+  const h1 = document.createElement("h1");
 
-  // Add event listener to submit button (optional)
-  submitBtn.addEventListener("click", () =>
-    form.dispatchEvent(new Event("submit"))
+  // Create anchor element
+  const anchor = document.createElement("a");
+  anchor.href = "/";
+
+  // Create span element for "Link"
+  const span = document.createElement("span");
+  span.classList.add("link");
+  span.textContent = "Link";
+
+  // Append elements
+  anchor.appendChild(document.createTextNode("Snip"));
+  anchor.appendChild(span);
+  h1.appendChild(anchor);
+  header.appendChild(h1);
+
+  // Append header to body
+  document.body.appendChild(header);
+
+  // Create main element
+  const main = document.createElement("main");
+
+  // Create section element for link form
+  const section = document.createElement("section");
+  section.classList.add("link-form");
+
+  // Create label for "Generator"
+  const generatorLabel = document.createElement("label");
+  generatorLabel.textContent = "Generator:";
+
+  // Create select element for generator
+  const generatorSelect = document.createElement("select");
+  generatorSelect.name = "generator";
+  generatorSelect.id = "generator";
+
+  // Create options for generator select
+  const generatorOptions = ["owo", "gay", "zws", "sketchy"];
+  generatorOptions.forEach((option) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = option;
+    optionElement.textContent = option;
+    generatorSelect.appendChild(optionElement);
+  });
+
+  // Create div element for input container
+  const inputContainer = document.createElement("div");
+  inputContainer.classList.add("input-container");
+
+  // Create label for "Link"
+  const linkLabel = document.createElement("label");
+  linkLabel.setAttribute("for", "link-input");
+  linkLabel.classList.add("link-input--image");
+
+  // Create input element for link input
+  const linkInput = document.createElement("input");
+  linkInput.type = "text";
+  linkInput.id = "link-input";
+  linkInput.placeholder = "Enter your URL...";
+  linkInput.classList.add("link-input");
+
+  // Create button element for copy
+  const copyButton = document.createElement("button");
+  copyButton.type = "button";
+  copyButton.id = "input-copy-btn";
+  copyButton.setAttribute("aria-label", "Copy");
+
+  // Create icon element for copy button
+  const copyIcon = document.createElement("i");
+  copyIcon.classList.add("fa-solid", "fa-copy");
+
+  // Append icon to copy button
+  copyButton.appendChild(copyIcon);
+
+  // Append elements to input container
+  inputContainer.appendChild(linkLabel);
+  inputContainer.appendChild(linkInput);
+  inputContainer.appendChild(copyButton);
+
+  // Create label for "Metadata"
+  const metadataLabel = document.createElement("label");
+  metadataLabel.textContent = "Metadata:";
+
+  // Create select element for metadata
+  const metadataSelect = document.createElement("select");
+  metadataSelect.name = "metadata";
+  metadataSelect.id = "metadata";
+
+  // Create options for metadata select
+  const metadataOptions = [
+    "Owoify metadata",
+    "Proxy metadata",
+    "Ignore metadata",
+  ];
+  metadataOptions.forEach((option) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = option.split(" ")[0].toUpperCase();
+    optionElement.textContent = option;
+    metadataSelect.appendChild(optionElement);
+  });
+
+  // Create form element
+  const dynamicForm = document.createElement("form");
+  dynamicForm.classList.add("link-form");
+
+  // Create button element for submit
+  const submitButton = document.createElement("button");
+  submitButton.type = "submit"; // Change type to "submit"
+  submitButton.id = "submit-btn";
+  submitButton.setAttribute("aria-label", "Shorten");
+  submitButton.textContent = "Shorten";
+
+  // Append elements to form
+  dynamicForm.appendChild(generatorLabel);
+  dynamicForm.appendChild(generatorSelect);
+  dynamicForm.appendChild(inputContainer);
+  dynamicForm.appendChild(metadataLabel);
+  dynamicForm.appendChild(metadataSelect);
+  dynamicForm.appendChild(submitButton);
+
+  // Add event listener for form submission
+  dynamicForm.addEventListener("submit", handleSubmit);
+
+  // Append form to section
+  section.appendChild(dynamicForm);
+
+  // Append section to main
+  main.appendChild(section);
+
+  // Append main to body
+  document.body.appendChild(main);
+
+  // Create footer element
+  const footer = document.createElement("footer");
+  footer.classList.add("footer-info");
+
+  // Create paragraph element for copyright
+  const copyrightParagraph = document.createElement("p");
+
+  // Create copyright text
+  const copyrightText = document.createTextNode(
+    `© ${new Date().getFullYear()} - Made with love by `
   );
 
-  // Update year in copyright every year
-  const currentYear = new Date().getFullYear();
-  const copyrightElement = document.querySelector("footer p");
-  const yearRegex = /\d+(?= \u00A9)/;
-  copyrightElement.textContent = copyrightElement.textContent.replace(
-    yearRegex,
-    currentYear
-  );
-  setInterval(() => {
-    const currentDate = new Date();
-    const nextYear =
-      currentDate.getFullYear() + (currentDate.getMonth() < 6 ? -1 : 0);
-    copyrightElement.textContent = copyrightElement.textContent.replace(
-      yearRegex,
-      nextYear.toString()
-    );
-  }, 86400000); // update every day (24 hours)
+  // Create anchor element for Marcy's GitHub profile
+  const githubAnchor = document.createElement("a");
+  githubAnchor.href = "https://github.com/marcythany";
+  githubAnchor.textContent = "Marcy";
+
+  // Append elements to copyright paragraph
+  copyrightParagraph.appendChild(copyrightText);
+  copyrightParagraph.appendChild(githubAnchor);
+
+  // Create second paragraph element for powered by message
+  const poweredByParagraph = document.createElement("p");
+
+  // Create powered by text
+  const poweredByText = document.createTextNode("Powered by ");
+
+  // Create anchor element for owo.vc
+  const owoAnchor = document.createElement("a");
+  owoAnchor.href = "https://owo.vc";
+  owoAnchor.textContent = "owo.vc";
+
+  // Append elements to powered by paragraph
+  poweredByParagraph.appendChild(poweredByText);
+  poweredByParagraph.appendChild(owoAnchor);
+
+  // Append paragraphs to footer
+  footer.appendChild(copyrightParagraph);
+  footer.appendChild(poweredByParagraph);
+
+  // Append footer to body
+  document.body.appendChild(footer);
 });
